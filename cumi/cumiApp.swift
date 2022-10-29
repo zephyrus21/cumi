7b7ce7ee440f45fbb86707503ab800e0b1449dfd
zephyrus21 <pandey.piyus@gmail.com>
2022-10-29 17:10:24 +0530

    Initial Commit

---
+++ cumi/cumiApp.swift
@@ -0,0 +1,17 @@
+//
+//  cumiApp.swift
+//  cumi
+//
+//  Created by Piyush Pandey on 29/10/22.
+//
+
+import SwiftUI
+
+@main
+struct cumiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
